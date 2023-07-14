c2b323daad538ee0e8563fdaf536f04ce0de2fbc
nilsons20000 <nilsons20000@mail.ru>
2023-07-14 23:00:51 +0300

    method encoded and decoded add

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,19 +1,40 @@
 class LinksController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   @@links_hash = {}
 
-  def index
-    link = get_random_short_symbol
-    @@links_hash[link] = { "link" => link}
-    @variable = @@links_hash
+  def get_random_short_symbol
+    short_link_generate_five_symbol = rand(36**5).to_s(36)
+    if @@links_hash.has_key?(short_link_generate_five_symbol)
+      return get_random_short_symbol(links_hash)
+    end
+    short_link_generate_five_symbol
   end
 
-  def get_random_short_symbol
-    short_link = rand(36**5).to_s(36)
+  def encode
+    long_link = params[:long_link]
+    short_link = get_random_short_symbol
+    @@links_hash[short_link] = {
+      "long_link" => long_link,
+      "short_link" => short_link
+    } 
+    render json: { encoded_string: @@links_hash }
+  end
+
+
+  def decode
+    short_link = params[:short_link]
+    puts @@links_hash
     if @@links_hash.has_key?(short_link)
-      return get_random(links_hash)
+      puts short_link
+        @long_link_from_array = @@links_hash[short_link]
+        render json: { encoded_string: @long_link_from_array }
     end
-    short_link
+
+    
+
+
+    
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'links/index'
+  # get 'links/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root "links#index"
+   root "links#index"
+
+  post '/encode', to: 'links#encode'
+  post '/decode', to: 'links#decode'
 
  
 
