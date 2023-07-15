710d09ebe2fb0bb3cafe7850821aea0e46b3534e
nilsons20000 <nilsons20000@mail.ru>
2023-07-15 19:54:26 +0300

    return delete

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -6,7 +6,7 @@ class LinksController < ApplicationController
   def get_random_short_symbol
     short_link_generate_five_symbol = rand(36**5).to_s(36)
     if @@links_hash.has_key?(short_link_generate_five_symbol)
-      return get_random_short_symbol(links_hash)
+      get_random_short_symbol(links_hash)
     end
     short_link_generate_five_symbol
   end
@@ -18,22 +18,20 @@ class LinksController < ApplicationController
       "long_link" => long_link,
       "short_link" => URI.parse(request.original_url) + short_link
     } 
-    render json: { encoded_long_link_to_short: @@links_hash[short_link]}
+    render json: { encoded_long_link_to_short: @@links_hash[short_link]["short_link"]}
   end
-
 
   def decode
     short_link = params[:short_link]
     get_url_domain = URI.parse(request.original_url).host
-    link_merge_original_and_short = URI.parse(request.original_url) + short_link
-    get_short_symbol_after_domain = link_merge_original_and_short.request_uri.split("/").last
-     if @@links_hash.has_key?(get_short_symbol_after_domain)
-         @long_link_from_array = @@links_hash[get_short_symbol_after_domain]
-         render json: { decode_short_link_to_long: @long_link_from_array }
-     else
-         render json: { decode_short_link_to_long: "Not Found" }
-     end
-    
+    link_merge = URI.parse(request.original_url) + short_link
+    get_short_symbol_after_domain = link_merge.request_uri.split("/").last
+      if @@links_hash.has_key?(get_short_symbol_after_domain)
+        @long_link_from_array = @@links_hash[get_short_symbol_after_domain]
+        render json: { decode_short_link_to_long: @long_link_from_array["long_link"] }
+      else
+        render json: { decode_short_link_to_long: "Not Found" }
+      end
   end
 
 end
